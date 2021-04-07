5d568bb68b1fd8e7a9138eeca7be9a912df6edde
Sophie Shin <sophie@alloy.co>
2021-04-07 19:33:43 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,4 +1,29 @@
 //define functions here
+function getIt() {
+  $('p').click(function() {
+    alert ("Hey!")
+  })
+}
+
+function frameIt() {
+  $('img').load(function() {
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt() {
+  $('input').keydown(function(key) {
+    if(key.which == 71){
+      alert('G was pressed')
+    }
+    })
+}
+
+function submitIt() {
+  $('form').submit(function() {
+    alert ('Your form is going to be submitted now.')
+  })
+}
 
 $(document).ready(function(){
 
